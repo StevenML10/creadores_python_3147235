ce27be98e00702da19f2688b5abcf4ca2c94a889
Dina <monroydina880@gmail.com>
2025-02-28 09:42:00 -0500

    parentesis

--- operadores.py
+++ operadores.py
@@ -2,17 +2,31 @@
 '''
 Los operadores en python
 siguen el siguiente orden jerarquico
-1. / , * , %
-2. + , -
-3. =
+
+1. (   )
+2. **
+3. / , * , %
+4. + , -
+5. =
+
+NOTA#1
 Si hay operaciones en el mismo nivel de jerarquia,
 se resuelven de izquierda a derecha.
+
+NOTA#2
+Si hay prentesis dentro del parentesis se resuelve primero el parentesis interno.
 '''
 
 a = 3
 b = 2
 c = 1
+x = 5
 
-y = a ** 2 - a * b - 1/ 3 + c
+"y = a ** 2 - a * b - 1/ 3 + c"
 
+"y = a * 2 ** 3 - a + c - c ** 3 + c"
+
+"y= a ** 2 * 3 / (c - x)"
+
+y = ((2 * a + c) / 7) * ( a+ (4 * a) / c)
 print(y)
