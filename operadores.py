cf7aa6752885c4a0282bafea2f85e2cfe83894c5
camm <romeroninojuancamil0000@gmail.com>
2025-03-07 08:58:41 -0500

    operadores booleanos

--- operadores.py
+++ operadores.py
@@ -17,16 +17,40 @@ NOTA#2
 Si hay prentesis dentro del parentesis se resuelve primero el parentesis interno.
 '''
 
-a = 3
-b = 2
-c = 1
-x = 5
-
 "y = a ** 2 - a * b - 1/ 3 + c"
 
 "y = a * 2 ** 3 - a + c - c ** 3 + c"
 
 "y= a ** 2 * 3 / (c - x)"
 
-y = ((2 * a + c) / 7) * ( a+ (4 * a) / c)
+"y = ((2 * a + c) / 7) * ( a+ (4 * a) / c)"
+
+
+'''
+OPERADORES RELACIONALES:
+Las operaciones aritmeticas
+resultan en un valor numerico:
+Las operaciones relacionales
+resultan en un valor booleano:
+True False (V, F SI NO)
+Operadores Relacionales:
+> , < , >= , <= , != , ==
+JERARQUIA DE OPERADORES
+(incluyendo Relacionales):
+
+1. (  )
+2.  **
+3. * , / , %
+4. + , -
+5. < , > , >= , <= , != , ==
+6. =
+'''
+a=2
+b=3
+c=7
+x=5
+h=1
+
+y = c / (x + 2) < c * a - c + 1 - b * 2
+
 print(y)
---
+++ operadores_logicos.py
@@ -0,0 +1,10 @@
+'''
+
+
+
+'''
+
+op1 = False
+op2 = False
+op3 = not op1
+print(op3)
